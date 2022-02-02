d718e49d4cc4a5182d30eaecc1131f8317655190
Laravelka <laravel.coder@yandex.ru>
2022-02-02 10:15:08 +0300

    fix config

--- src/main/java/ru/laravelka/VkChat/VkChat.java
+++ src/main/java/ru/laravelka/VkChat/VkChat.java
@@ -93,7 +93,6 @@ public class VkChat extends JavaPlugin {
                         );
                         return false;
                     }
-                    log.info("(false) word: " + forbiddenWord + " | " + message);
                 }
 
                 if (this.sendToChat(prefix + message)) {
